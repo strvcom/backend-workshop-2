8bcb4575ad0f3fc35e3893a5da6359a6c932c121
Robert Rossmann <rr.rossmann@me.com>
2015-11-21 00:10:30 +0100

    Implement Firebase

--- client/Chat.jsx
+++ client/Chat.jsx
@@ -2,19 +2,52 @@ import React from 'react'
 import MessageList from './MessageList.jsx'         // eslint-disable-line no-unused-vars
 import AddMessageForm from './AddMessageForm.jsx'   // eslint-disable-line no-unused-vars
 
+import Firebase from 'firebase'
 
 export default class Chat extends React.Component {
 
   constructor(props) {
     super(props)
 
+    this.fb = new Firebase('backend-meetup.firebaseio.com')
     this.state = {
       messages: []
     }
   }
 
   componentDidMount() {
-    // Insert updating magic here
+    const messages = this.fb.child('/messages')
+
+    messages.on('child_added', snapshot => {
+      const rawData = snapshot.val()
+      const message = {
+        id: snapshot.key(),
+        text: rawData.text,
+        user: rawData.user,
+        date: new Date(rawData.date)
+      }
+
+      this.addMessage(message)
+    })
+
+    messages.on('child_removed', snapshot => {
+      const key = snapshot.key()
+
+      this.state.messages = this.state.messages.filter(item => item.id !== key)
+
+      this.setState(this.state)
+    })
+
+    messages.on('child_changed', snapshot => {
+      const key = snapshot.key()
+
+      // Find the message with this key
+      const message = this.state.messages.filter(item => item.id === key)[0]
+
+      message.text = snapshot.val().text
+
+      this.setState(this.state)
+    })
   }
 
   addMessage(message) {
@@ -37,6 +70,8 @@ export default class Chat extends React.Component {
       user: this.props.user,
       date: new Date().toISOString()
     }
+
+    this.fb.child('/messages').push(message)
   }
 
   render() {
